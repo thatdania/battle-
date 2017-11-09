b5eeea4a717c866a6f71b5471dfef51c5157ca07
thatdania@gmail.com <thatdania@gmail.com>
2017-11-09 10:56:16 +0000

    Added Player 1 attacking Player 2

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -13,7 +13,6 @@ describe 'Battle', :type => :feature do
   it 'Player 1 gets confirmation of attacking Player 2' do
     sign_in_and_play
     click_on('AttackP2')
-    save_and_open_page
     expect(page).to have_content "Allan! You have attacked Dania!"
   end
 end
